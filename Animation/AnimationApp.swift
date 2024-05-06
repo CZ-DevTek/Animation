232726adecb48881ba7e7706ec1e90c984b8ed4f
Carlos García Pérez <carlosziok@gmail.com>
2024-05-06 22:24:37 +0300

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Carlos Garcia Perez on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
